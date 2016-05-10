3a3e4853225860972ca0a8bb710576fe432e7ee0
Qballl <qvanbooven513@gmail.com>
2016-05-10 18:41:35 +0000

    Updating the sign class to allow signs in nether/end

--- src/me/Qball/Wild/Listeners/SignChange.java
+++ src/me/Qball/Wild/Listeners/SignChange.java
@@ -1,10 +1,8 @@
 package me.Qball.Wild.Listeners;
 
 import me.Qball.Wild.Wild;
-
+import me.Qball.Wild.Utils.Checks;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.block.Biome;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -13,41 +11,42 @@ import org.bukkit.plugin.Plugin;
 public class SignChange implements Listener {
 	public static Plugin wild = Wild.getInstance();
 	@EventHandler
-	public void onSignChange(SignChangeEvent player) {
+	public void onSignChange(SignChangeEvent e) {
 		String Message = wild.getConfig().getString("No-Perm");
-		Location loc = player.getPlayer().getLocation();
-		int x = loc.getBlockX();
-		int z = loc.getBlockZ();
-		if (player.getLine(1).equalsIgnoreCase("[wild]")&& 
-				player.getLine(0).equalsIgnoreCase("wildtp"))
+
+		if (e.getLine(1).equalsIgnoreCase("[wild]")&& 
+				e.getLine(0).equalsIgnoreCase("wildtp"))
 						{
-			if(player.getPlayer().hasPermission("wild.wildtp.create.sign"))
+			if(e.getPlayer().hasPermission("wild.wildtp.create.sign"))
 			{
-			if (player.getPlayer().getWorld().getBiome(x, z) == Biome.HELL) {
-				player.getPlayer().sendMessage(ChatColor.RED+ "Signs cannot be put in the nether");
-				player.getBlock().breakNaturally();
-				player.setCancelled(true);
-			} else {
-				if (player.getPlayer().getWorld().getBiome(x, z) == Biome.SKY)
-					{
-					player.getPlayer().sendMessage(ChatColor.RED+ "Signs cannot be put in the end");
-					player.getBlock().breakNaturally();
-					player.setCancelled(true);
-				} else {
-					player.setLine(0, "§4====================");
-					player.setLine(1, "[§1Wild§0]");
-					player.setLine(2, "§4====================");
-					player.getPlayer().sendMessage(ChatColor.GREEN+ "Successfully made a new WildTP sign");
+			if(Checks.World(e.getPlayer()))
+				
+				 {
+					e.setLine(0, "§4====================");
+					e.setLine(1, "[§1Wild§0]");
+					e.setLine(2, "§4====================");
+					e.getPlayer().sendMessage(ChatColor.GREEN+ "Successfully made a new WildTP sign");
 				}
+			else
+			{
+				e.getPlayer().sendMessage(ChatColor.RED+"Signs cannot be placed in this world as the command isnt allowed in this world");
+				e.getBlock().breakNaturally();
+				e.setCancelled(true);
+				
 			}
+			}
+				else 
+				{
+			e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes((char) '&',Message));
+			e.setCancelled(true);
+				}
+			}	
+	
+		
+			
 		}
-			else 
-		{
-			player.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes((char) '&',Message));
-			player.setCancelled(true);
-		}
-		}
+	}
 			
 
-	}
-}
+	
+
