b1d985a75ebd0188af8032bf44c8374fe0e10a9e
Qballl <qvanbooven513@gmail.com>
2016-05-25 12:38:46 +0000

    Updating the NPE bug fix

--- src/me/Qball/Wild/Wild.java
+++ src/me/Qball/Wild/Wild.java
@@ -496,11 +496,7 @@ public class Wild extends JavaPlugin implements Listener {
 		@SuppressWarnings("unchecked")
 		List<String> potions = ((List<String>) plugin.getConfig().getList("Potions"));
 		int size  = potions.size();
-		if(size == 0)
-		{
-			return;
-		}
-		else
+		if(size != 0)
 		{
 		for(int i = 0; i <= size-1 ; i++)
 		{
