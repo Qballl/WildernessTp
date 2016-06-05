b71ab7eddc911668a1a2a86000d1ae950ecdaf9f
Qballl <qvanbooven513@gmail.com>
2016-06-05 21:33:44 +0000

    Teleport cancel event finished

--- src/me/Qball/Wild/Listeners/PlayMoveEvent.java
+++ src/me/Qball/Wild/Listeners/PlayMoveEvent.java
@@ -20,10 +20,13 @@ public void onMove(PlayerMoveEvent e)
 {
 	if (TeleportTar.CmdUsed.contains(e.getPlayer().getUniqueId()))
 	{
+		e.setCancelled(true);
+		e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("CancelMsg")));
+		TeleportTar.CmdUsed.remove(e.getPlayer().getUniqueId());
 		e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("CancelMsg")));
 		if(!moved.contains(e.getPlayer().getUniqueId())){
 			moved.add(e.getPlayer().getUniqueId());
-			TeleportTar.CmdUsed.remove(e.getPlayer().getUniqueId());
+			
 			}
 		
 		
