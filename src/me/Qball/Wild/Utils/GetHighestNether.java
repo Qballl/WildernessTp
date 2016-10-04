d413643619fdd3ccbe01bfecba4f97dad4a7797a
Creeperzombi3 <kooly213@hotmail.com>
2016-10-04 18:50:16 -0500

    Unnecessary check
    The block is already checked for air, no need to check if it is a liquid.

--- src/me/Qball/Wild/Utils/GetHighestNether.java
+++ src/me/Qball/Wild/Utils/GetHighestNether.java
@@ -16,8 +16,7 @@ public class GetHighestNether {
 			 if(target.getWorld().getBlockAt(tempx, Y, tempz).isEmpty())
 			 {
 				 Location loc = new Location(target.getWorld(),tempx,Y,tempz,0.0F,0.0F);
-				 if(!target.getWorld().getBlockAt(tempx, Y, tempz).isLiquid()
-						 && target.getWorld().getBlockAt(tempx, loc.getBlockY()-2, tempz).isEmpty()
+				 if(target.getWorld().getBlockAt(tempx, loc.getBlockY()-2, tempz).isEmpty()
 						 && !target.getWorld().getBlockAt(tempx,loc.getBlockY()-3, tempz).isEmpty()
 						 && !target.getWorld().getBlockAt(tempx,loc.getBlockY()-2,tempz).isLiquid())
 				 {
