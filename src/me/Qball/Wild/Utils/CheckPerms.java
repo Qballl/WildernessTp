fac0a73238f6a382cf549a304796e8125a6cf49c
Qballl <qvanbooven513@gmail.com>
2016-10-10 16:12:19 -0500

    Something something something darkside

--- src/me/Qball/Wild/Utils/CheckPerms.java
+++ src/me/Qball/Wild/Utils/CheckPerms.java
@@ -1,14 +1,14 @@
 package me.Qball.Wild.Utils;
 
 
+import me.Qball.Wild.Wild;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
-import org.bukkit.ChatColor;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
-
-import me.Qball.Wild.Wild;
 
 public class CheckPerms {
 	private final Wild wild;
@@ -20,7 +20,8 @@ public class CheckPerms {
 	public Economy econ = rsp.getProvider();
 	public void check(Player p)
 	{
-		
+		WildTpBack back = new WildTpBack();
+		back.saveLoc(p, p.getLocation());
 		int cost = wild.getConfig().getInt("Cost");
 		String costMSG = wild.getConfig().getString("Costmsg");
 		String strCost = String.valueOf(cost);
@@ -97,5 +98,87 @@ public class CheckPerms {
 		}
 		
 	}
+	public void check(Player p,Player target)
+	{
+		WildTpBack back = new WildTpBack();
+		back.saveLoc(target, target.getLocation());
+		int cost = wild.getConfig().getInt("Cost");
+		String costMSG = wild.getConfig().getString("Costmsg");
+		String strCost = String.valueOf(cost);
+		String costMsg = costMSG.replaceAll("\\{cost\\}", strCost);
+		int cool = wild.getConfig().getInt("Cooldown");
+		String Cool = String.valueOf(cool);
+		String coolmsg = wild.getConfig().getString("Cooldownmsg");
+		GetRandomLocation random = new GetRandomLocation(wild);
+		if(p.hasPermission("wild.wildtp.cost.bypass")&&p.hasPermission("wild.wildtp.cooldown.bypass"))
+			random.getWorldInfo(p);
+		if(p.hasPermission("wild.wildtp.cost.bypass")&&!p.hasPermission("wild.wildtp.cooldown.bypass"))
+		{
+			if(Wild.check(p))
+			{
+				random.getWorldInfo(target);
+				target.sendMessage(ChatColor.GREEN+ p.getDisplayName() + " throw you to radom location");
+			}
+			else
+			{
+				if(coolmsg.contains("{cool}"))
+					coolmsg = coolmsg.replace("{cool}", Cool);
+				else if(coolmsg.contains("{rem}"))
+					coolmsg = coolmsg.replace("{rem}", String.valueOf(Wild.getRem(p)));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', coolmsg));
+			}
+		}
+		if(!p.hasPermission("wild.wildtp.cost. bypass")&&p.hasPermission("wild.wildtp.cooldown.bypass"))
+		{
+			if(econ.getBalance(p) >= cost)
+			{
+				EconomyResponse r =econ.withdrawPlayer(p, cost);
+				if(r.transactionSuccess())
+				{
+					random.getWorldInfo(target);
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', costMsg));
+					target.sendMessage(ChatColor.GREEN+ p.getDisplayName() + " throw you to radom location");
 
+				}
+				else
+				{
+					p.sendMessage(ChatColor.RED + "Something has gone wrong sorry but we will be unable to teleport you :( ");
+				}
+			}
+			else
+				p.sendMessage(ChatColor.RED + "You do not have enough money to use this command");
+		}
+		if(!p.hasPermission("wild.wildtp.cost.bypass")&&!p.hasPermission("wild.wildtp.cooldown.bypass"))
+		{
+			if(!Wild.check(p))
+			{
+				if(coolmsg.contains("{cool}"))
+					coolmsg = coolmsg.replace("{cool}", Cool);
+				else if(coolmsg.contains("{rem}"))
+					coolmsg = coolmsg.replace("{rem}", String.valueOf(Wild.getRem(p)));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', coolmsg));
+			}
+			else
+			{
+				if(econ.getBalance(p) >= cost)
+				{
+					EconomyResponse r =econ.withdrawPlayer(p, cost);
+					if(r.transactionSuccess())
+					{
+						random.getWorldInfo(target);
+						p.sendMessage(ChatColor.translateAlternateColorCodes('&', costMsg));
+						target.sendMessage(ChatColor.GREEN+ p.getDisplayName() + " throw you to radom location");
+
+					}
+					else
+					{
+						p.sendMessage(ChatColor.RED + "Something has gone wrong sorry but we will be unable to teleport you :( ");
+					}
+				}
+				else
+					p.sendMessage(ChatColor.RED + "You do not have enough money to use this command");
+			}
+		}
+		
+	}
 }
