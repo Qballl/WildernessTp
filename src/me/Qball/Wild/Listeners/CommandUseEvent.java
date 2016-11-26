18d6e59b97674be9e3d6baa95f447dfa06266eed
BuildTools <qvanbooven513@gmail.com>
2016-11-25 18:22:39 -0600

    Added FBasics override

--- src/me/Qball/Wild/Listeners/CommandUseEvent.java
+++ src/me/Qball/Wild/Listeners/CommandUseEvent.java
@@ -2,31 +2,39 @@ package me.Qball.Wild.Listeners;
 
 import java.util.List;
 
+import me.Qball.Wild.Utils.CheckPerms;
+import me.Qball.Wild.Utils.Checks;
 import me.Qball.Wild.Wild;
 import me.Qball.Wild.Utils.TeleportTarget;
 
 import  org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CommandUseEvent {
-	private Wild wild= Wild.getInstance();
-	public void onCmd(PlayerCommandPreprocessEvent e)
-	{
-		String command = e.getMessage().toLowerCase(); 
-		List<String> blockedCmds = wild.getConfig().getStringList("BlockCommands");
-		if(!TeleportTarget.cmdUsed.contains(e.getPlayer().getUniqueId())) {
-				return;
-			}
-		for(String cmd : blockedCmds) {
-			if(command.contains(cmd)); {
-				e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("Block_Command_Message")));
-				e.setCancelled(true);
-				break;
-			}
-		} 
-		
-	}
-	 
+	private Wild wild = Wild.getInstance();
 
+	public void onCmd(PlayerCommandPreprocessEvent e) {
+		String command = e.getMessage().toLowerCase();
+		List<String> blockedCmds = wild.getConfig().getStringList("BlockCommands");
+		if (TeleportTarget.cmdUsed.contains(e.getPlayer().getUniqueId())) {
+			for (String cmd : blockedCmds) {
+				if (command.contains(cmd)) ;
+				{
+					e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("Block_Command_Message")));
+					e.setCancelled(true);
+					break;
+				}
+			}
+		}
+		else if(e.getMessage().equalsIgnoreCase("wild")&&wild.getConfig().getBoolean("FBasics")) {
+			CheckPerms check = new CheckPerms(wild);
+			Checks checks = new Checks(wild);
+			Player p = e.getPlayer();
+				if(!checks.world(p))
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&',wild.getConfig().getString("WorldMsg")));
+				else
+					check.check(p);
+		}
+	}
 }
- 
