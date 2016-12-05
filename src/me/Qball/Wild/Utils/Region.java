3585d1a0a0957d174c8d1f414ed064a9a849fc06
BuildTools <qvanbooven513@gmail.com>
2016-12-04 20:57:58 -0600

    Setting up for independt portal system and claim checking

---
+++ src/me/Qball/Wild/Utils/Region.java
@@ -0,0 +1,13 @@
+package me.Qball.Wild.Utils;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class Region {
+    private Vector vec1;
+    private Vector vec2;
+    public Region(Vector vec1,Vector vec2){
+     this.vec1 = vec1;
+     this.vec2 = vec2;
+    }
+}
