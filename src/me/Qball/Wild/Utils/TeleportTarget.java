59ceeaa4b3dbba7ea63000bb271c5ec00ba2bc20
BuildTools <qvanbooven513@gmail.com>
2017-01-31 08:29:40 -0600

    Working on rewriting to make this easier to read

--- src/me/Qball/Wild/Utils/TeleportTarget.java
+++ src/me/Qball/Wild/Utils/TeleportTarget.java
@@ -22,242 +22,257 @@ public class TeleportTarget {
 		random = new GetRandomLocation(plugin);
 		check = new Checks(wild);
 	}
-    public static ArrayList<UUID> cmdUsed = new ArrayList<UUID>();
-    
-   
-    
-  public void TP(final Location loc, final Player target)
-    {		    
-	    TeleportTarget teleportTarget = new TeleportTarget(wild);
-	  	int confWait = wild.getConfig().getInt("Wait");
-    	if (cmdUsed.contains(target.getUniqueId()))
-    	{
-    		target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("UsedCmd")));
-    	}
-    	else 
-    	{
-    		 cmdUsed.add(target.getUniqueId()); 
-    		 String Wait = String.valueOf(confWait);
-    	     String delayMsg = wild.getConfig().getString("WaitMsg");
-    	     String DelayMsg = delayMsg.replaceAll("\\{wait}", Wait);
-    	    String location = String.valueOf(loc.getBlockX()) + " " + String.valueOf(loc.getBlockY()) + " " + String.valueOf(loc.getBlockZ());
- 			final String Teleport = wild.getConfig().getString("Teleport").replace("<loc>",location);
-	        int wait = confWait*20;
-	        if(wild.getConfig().getBoolean("Play"))
-	        {
-	        if (wait >0) {
-	        	if(wild.portalUsed.contains(target.getUniqueId()))
-	        	{
-	        		if(!check.blacklistBiome(loc))
-                	{
-                	 cmdUsed.remove(target.getUniqueId());
-                	 Wild.applyPotions(target);
-                     target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
-                     target.sendMessage(ChatColor.translateAlternateColorCodes('&',Teleport));
- 					 target.playSound(loc, Sounds.getSound(), 3, 10);
- 					 teleportTarget.doCommands(target);
- 					 if(Wild.cancel.contains(target.getUniqueId())){
-							Wild.cancel.remove(target.getUniqueId());
-						}
- 					 }
-	        	}
-	        	else{
-	            target.sendMessage(ChatColor.translateAlternateColorCodes('&',DelayMsg));
+	public final static ArrayList<UUID> cmdUsed = new ArrayList<UUID>();
 
-	            new BukkitRunnable() {
-	                public void run() {
-	                	if(!PlayMoveEvent.moved.contains(target.getUniqueId())&& !PlayMoveEvent.dontTele.contains(target.getUniqueId()))
-	                			{        			
-	                	if(!check.blacklistBiome(loc))
-	                	{
-	                	 cmdUsed.remove(target.getUniqueId());
-	                	 Wild.applyPotions(target);
-	                	 target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
-	                     target.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(ChatColor.translateAlternateColorCodes((char) '&', Teleport)).toString());
-	 					 target.playSound(loc, Sounds.getSound(), 3, 10);
-	 					 teleportTarget.doCommands(target);
-	 					 if(Wild.cancel.contains(target.getUniqueId())){
-							Wild.cancel.remove(target.getUniqueId());
-						 }
-	 					 if(PlayMoveEvent.moved.contains(target.getUniqueId()))
-	 					   {
-	 						   PlayMoveEvent.moved.remove(target.getUniqueId());
-	 					   }
-	 					   if(wild.portalUsed.contains(target.getUniqueId()))
-	 						   wild.portalUsed.remove(target.getUniqueId());
-	                	}
-	                	else
-	                	{
-	                		if(wild.retries!=0)
-	                		{
-	                			String info = random.getWorldInfomation(target);
-	                        	random.recallTeleport(random.getRandomLoc(info, target), target);
-	                		}
-	                		else
-	               		 {
-	               			 target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("No Suitable Location")));
-	               		 }
-	                	}
-	             }
-	                	else
-        				{	
-	                		if(PlayMoveEvent.moved.contains(target.getUniqueId()))
-	                		{
-        					PlayMoveEvent.moved.remove(target.getUniqueId());
-	                		}
-	                		else if (PlayMoveEvent.dontTele.contains(target.getUniqueId()))
-	                		{
-	                			PlayMoveEvent.dontTele.remove(target.getUniqueId());
-	                		}
-	                		else if(wild.portalUsed.contains(target.getUniqueId()))
-	                			wild.portalUsed.remove(target.getUniqueId());
-        				
-        				}
-	                }
-	            }.runTaskLater(wild, wait);
-	        	}
-	        }  
-	        
-	        else
-	        {
-	        	if(!check.blacklistBiome(loc))
-	        	{
-                    cmdUsed.remove(target.getUniqueId());
-	        	    Wild.applyPotions(target);
-	        	    target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
-	                target.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(ChatColor.translateAlternateColorCodes((char) '&', Teleport)).toString());
-				    target.playSound(loc, Sounds.getSound(), 3, 10);
-				    teleportTarget.doCommands(target);
-				if(Wild.cancel.contains(target.getUniqueId())){
-					Wild.cancel.remove(target.getUniqueId());
-				}
-				if(wild.portalUsed.contains(target.getUniqueId()))
-	            	wild.portalUsed.remove(target.getUniqueId());
-	        	}
-	        	else
-	        	{
-	        		if(wild.retries!=0)
-	        		{
-                	String info = random.getWorldInfomation(target);
-                	random.recallTeleport(random.getRandomLoc(info, target), target);
-	        		}
-	        		else
-	       		 {
-	       			 target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("No Suitable Location")));
-	       		 }
-	        	}
-				
-    	}
-	        } 
-	        else
-	        {
-	        	if(wait>0)
-	        	{
-	        		if(wild.portalUsed.contains(target.getUniqueId()))
-		        	{
-		        		if(!check.blacklistBiome(loc))
-	                	{
-	                	 cmdUsed.remove(target.getUniqueId());
-	                	 Wild.applyPotions(target);
-	                	 target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
-	                     target.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(ChatColor.translateAlternateColorCodes((char) '&', Teleport)).toString());
-	                     teleportTarget.doCommands(target);
-	                     if(Wild.cancel.contains(target.getUniqueId())){
+	public void teleport(Location loc, Player p){
+		if(cmdUsed.contains(p)){
+			p.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("UsedCmd")));
+		}else{
+			int confWait = wild.getConfig().getInt("Wait");
+			cmdUsed.add(p.getUniqueId());
+			String Wait = String.valueOf(confWait);
+			String delayMsg = wild.getConfig().getString("WaitMsg");
+			delayMsg = delayMsg.replaceAll("\\{wait}", Wait);
+			String location = String.valueOf(loc.getBlockX()) + " " + String.valueOf(loc.getBlockY()) + " " + String.valueOf(loc.getBlockZ());
+			String teleport = wild.getConfig().getString("Teleport").replace("<loc>",location);
+			int wait = confWait*20;
+			if(wait>0){
+
+			}
+		}
+	}
+	public void TP(final Location loc, final Player target)
+	{
+		TeleportTarget teleportTarget = new TeleportTarget(wild);
+		int confWait = wild.getConfig().getInt("Wait");
+		if (cmdUsed.contains(target.getUniqueId()))
+		{
+			target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("UsedCmd")));
+		}
+		else
+		{
+			cmdUsed.add(target.getUniqueId());
+			String Wait = String.valueOf(confWait);
+			String delayMsg = wild.getConfig().getString("WaitMsg");
+			String DelayMsg = delayMsg.replaceAll("\\{wait}", Wait);
+			String location = String.valueOf(loc.getBlockX()) + " " + String.valueOf(loc.getBlockY()) + " " + String.valueOf(loc.getBlockZ());
+			final String Teleport = wild.getConfig().getString("Teleport").replace("<loc>",location);
+			int wait = confWait*20;
+			if(wild.getConfig().getBoolean("Play"))
+			{
+				if (wait >0) {
+					if(wild.portalUsed.contains(target.getUniqueId()))
+					{
+						if(!check.blacklistBiome(loc))
+						{
+							cmdUsed.remove(target.getUniqueId());
+							Wild.applyPotions(target);
+							target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
+							target.sendMessage(ChatColor.translateAlternateColorCodes('&',Teleport));
+							target.playSound(loc, Sounds.getSound(), 3, 10);
+							teleportTarget.doCommands(target);
+							if(Wild.cancel.contains(target.getUniqueId())){
 								Wild.cancel.remove(target.getUniqueId());
 							}
-	 					 }
-		        	}
-	        		else
-		            target.sendMessage(ChatColor.translateAlternateColorCodes('&',DelayMsg));
-	        		new BukkitRunnable()
-	        		{
-	        			public void run()
-	        			{
-	        				if(!PlayMoveEvent.moved.contains(target.getUniqueId()) && !PlayMoveEvent.dontTele.contains(target.getUniqueId()))
-	        				{
-	        				if(!check.blacklistBiome(loc))
-	        				{
-	        				cmdUsed.remove(target.getUniqueId());
-	        				 Wild.applyPotions(target);
-	        				 target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
-		                     target.sendMessage(ChatColor.translateAlternateColorCodes((char) '&', Teleport));
-		                     teleportTarget.doCommands(target);
-		                     if(Wild.cancel.contains(target.getUniqueId())){
-									Wild.cancel.remove(target.getUniqueId());
-								}
-		 					  if(PlayMoveEvent.moved.contains(target.getUniqueId()))
-		 					   {
-		 						   PlayMoveEvent.moved.remove(target.getUniqueId());
-		 					   }
-		 					  if(wild.portalUsed.contains(target.getUniqueId()))
-		 						  wild.portalUsed.remove(target.getUniqueId());
-	        				}
-	        				else
-	        				{ 
-	        					if(wild.retries!=0)
-	        					{
-	        					String info = random.getWorldInfomation(target);
-	        	               	random.recallTeleport(random.getRandomLoc(info, target), target);
-	        					}
-	        					else
-	        					 {
-	        						 target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("No Suitable Location")));
-	        					 }
-	        				}
-	        				
-	        			}
-	        				else
-	        				{
+						}
+					}
+					else{
+						target.sendMessage(ChatColor.translateAlternateColorCodes('&',DelayMsg));
 
-		                		if(PlayMoveEvent.moved.contains(target.getUniqueId()))
-		                		{
-	        					PlayMoveEvent.moved.remove(target.getUniqueId());
-		                		}
-		                		else if (PlayMoveEvent.dontTele.contains(target.getUniqueId()))
-		                		{
-		                			PlayMoveEvent.dontTele.remove(target.getUniqueId());
-		                		}
-		                		else if(wild.portalUsed.contains(target.getUniqueId()))
-		                			wild.portalUsed.remove(target.getUniqueId());
-	        				}
-	        			}
-	        		}.runTaskLater(wild, wait);
-	        	}
-	        	else
-	        	{
+						new BukkitRunnable() {
+							public void run() {
+								if(!PlayMoveEvent.moved.contains(target.getUniqueId())&& !PlayMoveEvent.dontTele.contains(target.getUniqueId()))
+								{
+									if(!check.blacklistBiome(loc))
+									{
+										cmdUsed.remove(target.getUniqueId());
+										Wild.applyPotions(target);
+										target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
+										target.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(ChatColor.translateAlternateColorCodes((char) '&', Teleport)).toString());
+										target.playSound(loc, Sounds.getSound(), 3, 10);
+										teleportTarget.doCommands(target);
+										if(Wild.cancel.contains(target.getUniqueId())){
+											Wild.cancel.remove(target.getUniqueId());
+										}
+										if(PlayMoveEvent.moved.contains(target.getUniqueId()))
+										{
+											PlayMoveEvent.moved.remove(target.getUniqueId());
+										}
+										if(wild.portalUsed.contains(target.getUniqueId()))
+											wild.portalUsed.remove(target.getUniqueId());
+									}
+									else
+									{
+										if(wild.retries!=0)
+										{
+											String info = random.getWorldInfomation(target);
+											random.recallTeleport(random.getRandomLoc(info, target), target);
+										}
+										else
+										{
+											target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("No Suitable Location")));
+										}
+									}
+								}
+								else
+								{
+									if(PlayMoveEvent.moved.contains(target.getUniqueId()))
+									{
+										PlayMoveEvent.moved.remove(target.getUniqueId());
+									}
+									else if (PlayMoveEvent.dontTele.contains(target.getUniqueId()))
+									{
+										PlayMoveEvent.dontTele.remove(target.getUniqueId());
+									}
+									else if(wild.portalUsed.contains(target.getUniqueId()))
+										wild.portalUsed.remove(target.getUniqueId());
+
+								}
+							}
+						}.runTaskLater(wild, wait);
+					}
+				}
+
+				else
+				{
+					if(!check.blacklistBiome(loc))
+					{
+						cmdUsed.remove(target.getUniqueId());
+						Wild.applyPotions(target);
+						target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
+						target.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(ChatColor.translateAlternateColorCodes((char) '&', Teleport)).toString());
+						target.playSound(loc, Sounds.getSound(), 3, 10);
+						teleportTarget.doCommands(target);
+						if(Wild.cancel.contains(target.getUniqueId())){
+							Wild.cancel.remove(target.getUniqueId());
+						}
+						if(wild.portalUsed.contains(target.getUniqueId()))
+							wild.portalUsed.remove(target.getUniqueId());
+					}
+					else
+					{
+						if(wild.retries!=0)
+						{
+							String info = random.getWorldInfomation(target);
+							random.recallTeleport(random.getRandomLoc(info, target), target);
+						}
+						else
+						{
+							target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("No Suitable Location")));
+						}
+					}
+
+				}
+			}
+			else
+			{
+				if(wait>0)
+				{
+					if(wild.portalUsed.contains(target.getUniqueId()))
+					{
+						if(!check.blacklistBiome(loc))
+						{
+							cmdUsed.remove(target.getUniqueId());
+							Wild.applyPotions(target);
+							target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
+							target.sendMessage((new StringBuilder()).append(ChatColor.GREEN).append(ChatColor.translateAlternateColorCodes((char) '&', Teleport)).toString());
+							teleportTarget.doCommands(target);
+							if(Wild.cancel.contains(target.getUniqueId())){
+								Wild.cancel.remove(target.getUniqueId());
+							}
+						}
+					}
+					else
+						target.sendMessage(ChatColor.translateAlternateColorCodes('&',DelayMsg));
+					new BukkitRunnable()
+					{
+						public void run()
+						{
+							if(!PlayMoveEvent.moved.contains(target.getUniqueId()) && !PlayMoveEvent.dontTele.contains(target.getUniqueId()))
+							{
+								if(!check.blacklistBiome(loc))
+								{
+									cmdUsed.remove(target.getUniqueId());
+									Wild.applyPotions(target);
+									target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
+									target.sendMessage(ChatColor.translateAlternateColorCodes((char) '&', Teleport));
+									teleportTarget.doCommands(target);
+									if(Wild.cancel.contains(target.getUniqueId())){
+										Wild.cancel.remove(target.getUniqueId());
+									}
+									if(PlayMoveEvent.moved.contains(target.getUniqueId()))
+									{
+										PlayMoveEvent.moved.remove(target.getUniqueId());
+									}
+									if(wild.portalUsed.contains(target.getUniqueId()))
+										wild.portalUsed.remove(target.getUniqueId());
+								}
+								else
+								{
+									if(wild.retries!=0)
+									{
+										String info = random.getWorldInfomation(target);
+										random.recallTeleport(random.getRandomLoc(info, target), target);
+									}
+									else
+									{
+										target.sendMessage(ChatColor.translateAlternateColorCodes('&', wild.getConfig().getString("No Suitable Location")));
+									}
+								}
+
+							}
+							else
+							{
+
+								if(PlayMoveEvent.moved.contains(target.getUniqueId()))
+								{
+									PlayMoveEvent.moved.remove(target.getUniqueId());
+								}
+								else if (PlayMoveEvent.dontTele.contains(target.getUniqueId()))
+								{
+									PlayMoveEvent.dontTele.remove(target.getUniqueId());
+								}
+								else if(wild.portalUsed.contains(target.getUniqueId()))
+									wild.portalUsed.remove(target.getUniqueId());
+							}
+						}
+					}.runTaskLater(wild, wait);
+				}
+				else
+				{
 					cmdUsed.remove(target.getUniqueId());
-		        	Wild.applyPotions(target);
+					Wild.applyPotions(target);
 					target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));
-		            target.sendMessage(ChatColor.translateAlternateColorCodes((char) '&', Teleport));
+					target.sendMessage(ChatColor.translateAlternateColorCodes((char) '&', Teleport));
 					teleportTarget.doCommands(target);
-		            if(wild.portalUsed.contains(target.getUniqueId()))
+					if(wild.portalUsed.contains(target.getUniqueId()))
 						target.teleport(new Location(loc.getWorld(),loc.getBlockX(),loc.getBlockY()+3,loc.getBlockZ(),0.0F,0.0F));   	wild.portalUsed.remove(target.getUniqueId());
 					if(Wild.cancel.contains(target.getUniqueId())){
 						Wild.cancel.remove(target.getUniqueId());
 					}
-	        	}
-	        }
-	        }
-    	if(PlayMoveEvent.moved.contains(target.getUniqueId()))
-		   {
-			   PlayMoveEvent.moved.remove(target.getUniqueId());
-			   
-		   }
+				}
+			}
+		}
+		if(PlayMoveEvent.moved.contains(target.getUniqueId()))
+		{
+			PlayMoveEvent.moved.remove(target.getUniqueId());
+
+		}
 		else if (PlayMoveEvent.dontTele.contains(target.getUniqueId()))
 		{
 			PlayMoveEvent.dontTele.remove(target.getUniqueId());
 		}
 		else if(wild.portalUsed.contains(target.getUniqueId())) {
-        	wild.portalUsed.remove(target.getUniqueId());
-         }
-    }
-    private void doCommands(Player p){
-  		if(wild.getConfig().getString("PostCommand")==null)
-  			return;
-  		for(String command : wild.getConfig().getStringList("PostCommand")) {
-  			command = command.replaceAll("\\{player}",p.getDisplayName());
+			wild.portalUsed.remove(target.getUniqueId());
+		}
+	}
+	private void doCommands(Player p){
+		if(wild.getConfig().getString("PostCommand")==null)
+			return;
+		for(String command : wild.getConfig().getStringList("PostCommand")) {
+			command = command.replaceAll("\\{player}",p.getDisplayName());
 			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
 		}
 	}
-  
+
 }
