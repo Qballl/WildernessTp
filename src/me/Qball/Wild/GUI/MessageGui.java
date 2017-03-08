8d86a25cdb3e24fa4e475cc669345557ffeb2537
BuildTools <qvanbooven513@gmail.com>
2017-03-07 18:50:55 -0600

    Finished rewrite of the GUI

--- src/me/Qball/Wild/GUI/MessageGui.java
+++ src/me/Qball/Wild/GUI/MessageGui.java
@@ -25,10 +25,10 @@ public class MessageGui {
         Messages.setItem(2, makeItem("NoSuit", "Click to set the no suitable location message"));
         Messages.setItem(4, makeItem("CostMsg", "Click to set the cost message"));
         Messages.setItem(6, makeItem("No-Break", "Click to set the no perm for sign break message"));
-        Messages.setItem(8, NoPerm());
-        Messages.setItem(10, Cool());
-        Messages.setItem(12, WarmUp());
-        Messages.setItem(14, UsedCmd());
+        Messages.setItem(8, makeItem("No-Perm", "Click to set the No permission to make a sign message"));
+        Messages.setItem(10, makeItem( "Cooldown Message", "Click to set the cool down message"));
+        Messages.setItem(12, makeItem( "Wait/Warmup Message", "Click to set the warmp/delay/wait message"));
+        Messages.setItem(14, makeItem("Used command Message", "Click to set the command used message"));
         Messages.setItem(17, Close);
     }
     private static ItemStack makeItem(String name, String lore) {
@@ -37,51 +37,8 @@ public class MessageGui {
         ArrayList<String> loreArray = new ArrayList<>();
         loreArray.add(lore);
         meta.setLore(loreArray);
+        meta.setDisplayName(name);
         stack.setItemMeta(meta);
         return stack;
-    }
-
-    public static ItemStack NoPerm() {
-        ItemStack NoPerm = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = NoPerm.getItemMeta();
-        meta.setDisplayName("No-Perm");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the No permission to make a sign message");
-        meta.setLore(lore);
-        NoPerm.setItemMeta(meta);
-        return NoPerm;
-    }
-
-    public static ItemStack Cool() {
-        ItemStack Cool = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = Cool.getItemMeta();
-        meta.setDisplayName("Cooldown Message");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the cool down message");
-        meta.setLore(lore);
-        Cool.setItemMeta(meta);
-        return Cool;
-    }
-
-    public static ItemStack WarmUp() {
-        ItemStack Warm = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = Warm.getItemMeta();
-        meta.setDisplayName("Wait/WarmUp Message");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the warmp/delay/wait message");
-        meta.setLore(lore);
-        Warm.setItemMeta(meta);
-        return Warm;
-    }
-
-    public static ItemStack UsedCmd() {
-        ItemStack Use = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = Use.getItemMeta();
-        meta.setDisplayName("Used command Message");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the command used message");
-        meta.setLore(lore);
-        Use.setItemMeta(meta);
-        return Use;
     }
 }
--- src/me/Qball/Wild/GUI/SetGui.java
+++ src/me/Qball/Wild/GUI/SetGui.java
@@ -2,6 +2,7 @@ package me.Qball.Wild.GUI;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -23,128 +24,16 @@ public class SetGui {
         Inventory set = Bukkit.createInventory(p, 27, "WildTp");
         p.openInventory(set);
 
-        set.setItem(0, minX());
-        set.setItem(2, maxX());
-        set.setItem(4, minZ());
-        set.setItem(6, maxZ());
-        set.setItem(8, Cool());
-        set.setItem(10, Cost());
-        set.setItem(12, delay());
-        set.setItem(14, retry());
-        set.setItem(16, doRetry());
-        set.setItem(18, distance());
+        set.setItem(0, MainGui.makeItem(Material.BOOK_AND_QUILL, "MinX", Collections.singletonList("Click to set the minx")));
+        set.setItem(2, MainGui.makeItem(Material.BOOK_AND_QUILL, "MaxX", Collections.singletonList("Click to set the maxx")));
+        set.setItem(4, MainGui.makeItem(Material.BOOK_AND_QUILL, "MinZ", Collections.singletonList("Click to set the minz")));
+        set.setItem(6, MainGui.makeItem(Material.BOOK_AND_QUILL, "MaxZ", Collections.singletonList("Click to set the maxz")));
+        set.setItem(8, MainGui.makeItem(Material.WATCH, "Cooldown", Collections.singletonList("Click me to set the cooldown for the command")));
+        set.setItem(10, MainGui.makeItem(Material.GOLD_BLOCK, "Cost", Collections.singletonList("Click me to set the cost for the command")));
+        set.setItem(12, MainGui.makeItem(Material.WATCH, "Wait", Collections.singletonList("Click to set the wait before telepoting happens")));
+        set.setItem(14, MainGui.makeItem(Material.WATCH, "Retries", Collections.singletonList("Click to set the number of retries if a location is unsuitable")));
+        set.setItem(16, MainGui.makeItem(Material.WATCH, "Do Retry", Collections.singletonList("Click to set true or false for doing retries")));
+        set.setItem(18, MainGui.makeItem(Material.MAP, "Distance", Arrays.asList("Click to set the distance the plugin checks for a claim")));
         set.setItem(26, Close);
     }
-
-    public static ItemStack minX() {
-        ItemStack MinX = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = MinX.getItemMeta();
-        meta.setDisplayName("MinX");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the minx");
-        meta.setLore(lore);
-        MinX.setItemMeta(meta);
-        return MinX;
-    }
-
-    public static ItemStack maxX() {
-        ItemStack MaxX = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = MaxX.getItemMeta();
-        meta.setDisplayName("MaxX");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the maxx");
-        meta.setLore(lore);
-        MaxX.setItemMeta(meta);
-        return MaxX;
-    }
-
-    public static ItemStack minZ() {
-        ItemStack MinZ = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = MinZ.getItemMeta();
-        meta.setDisplayName("MinZ");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the minz");
-        meta.setLore(lore);
-        MinZ.setItemMeta(meta);
-        return MinZ;
-    }
-
-    public static ItemStack maxZ() {
-        ItemStack MaxZ = new ItemStack(Material.BOOK_AND_QUILL, 1);
-        ItemMeta meta = MaxZ.getItemMeta();
-        meta.setDisplayName("MaxZ");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the maxz");
-        meta.setLore(lore);
-        MaxZ.setItemMeta(meta);
-        return MaxZ;
-    }
-
-    public static ItemStack Cool() {
-        ItemStack Cool = new ItemStack(Material.WATCH, 1);
-        ItemMeta meta = Cool.getItemMeta();
-        meta.setDisplayName("Cooldown");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click me to set the cooldown for the command");
-        meta.setLore(lore);
-        Cool.setItemMeta(meta);
-        return Cool;
-    }
-
-    public static ItemStack Cost() {
-        ItemStack cost = new ItemStack(Material.GOLD_BLOCK, 1);
-        ItemMeta meta = cost.getItemMeta();
-        meta.setDisplayName("Cost");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click me to set the cost for the command");
-        meta.setLore(lore);
-        cost.setItemMeta(meta);
-        return cost;
-    }
-
-    public static ItemStack delay() {
-        ItemStack Wait = new ItemStack(Material.WATCH, 1);
-        ItemMeta meta = Wait.getItemMeta();
-        meta.setDisplayName("Wait");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the wait before telepoting happens");
-        meta.setLore(lore);
-        Wait.setItemMeta(meta);
-        return Wait;
-
-    }
-
-    public static ItemStack retry() {
-        ItemStack retry = new ItemStack(Material.WATCH, 1);
-        ItemMeta meta = retry.getItemMeta();
-        meta.setDisplayName("Retries");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set the number of retries if a location is unsuitable");
-        meta.setLore(lore);
-        retry.setItemMeta(meta);
-        return retry;
-
-    }
-
-    public static ItemStack doRetry() {
-        ItemStack retry = new ItemStack(Material.WATCH, 1);
-        ItemMeta meta = retry.getItemMeta();
-        meta.setDisplayName("Do Retry");
-        ArrayList<String> lore = new ArrayList<String>();
-        lore.add("Click to set true or false for doing retries");
-        meta.setLore(lore);
-        retry.setItemMeta(meta);
-        return retry;
-    }
-
-    public static ItemStack distance() {
-        ItemStack distance = new ItemStack(Material.MAP, 1);
-        ItemMeta meta = distance.getItemMeta();
-        meta.setDisplayName("Distance");
-        meta.setLore(Arrays.asList(new String[]{"Click to set the distance the plugin checks for a claim"}));
-        distance.setItemMeta(meta);
-        return distance;
-    }
-
-
 }
