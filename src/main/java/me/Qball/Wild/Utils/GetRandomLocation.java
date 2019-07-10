d6a84b1ec5ac22dd550eb464a6e69f16541a7ac4
Azoraqua <me@azoraqua.com>
2019-07-10 20:53:25 +0200

    Fixed issue related to spawning in blocks or oceans. As mentioned by Discord user "Barns#1757".

--- src/main/java/me/Qball/Wild/Utils/GetRandomLocation.java
+++ src/main/java/me/Qball/Wild/Utils/GetRandomLocation.java
@@ -1,5 +1,6 @@
 package me.Qball.Wild.Utils;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
@@ -7,6 +8,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import me.Qball.Wild.Wild;
 
@@ -66,8 +68,17 @@ public class GetRandomLocation {
             getRandomLoc(p, w, maxX, minX, maxZ, minZ);
         }*/
         Location loc = new Location(w, x+.5, y, z+.5, 0.0F, 0.0F);
-        wild.random(p, loc);
-        retries = 0;
+
+        if (loc.getBlock().isLiquid() || Arrays.stream(Biome.values()).filter(b -> b.name().contains("OCEAN")).anyMatch(b -> loc.getBlock().getBiome() == b)) {
+            getRandomLoc(p, w, maxX, minX, maxZ, minZ);
+        } else {
+            if (!loc.getBlock().isEmpty() || !loc.getBlock().isPassable()) {
+                loc.setY(loc.getY() + 1);
+            }
+
+            wild.random(p, loc);
+            retries = 0;
+        }
     }
 
     public String getWorldInformation(Location loc) {
