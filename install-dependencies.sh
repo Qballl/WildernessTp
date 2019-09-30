8adf8a50fe633a4ab26bcf02ad7e0ba1271a4e90
Qball <qvanbooven513@gmail.com>
2019-09-30 18:03:19 -0500

    Added economy and fix dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,3 +3,5 @@ mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/Towny.
 mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/FabledKingdoms.jar -DgroupId=com.songoda.kingdoms -DartifactId=FabledKingdoms -Dversion=1.2.6
 mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/Feudal.jar -DgroupId=us.forseth11 -DartifactId=Feudal -Dversion=1.7.3
 mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/FactionsUUID.jar -DgroupId=com.massivecraft -DartifactId=FactionsUUID -Dversion=1.6.9.5-U0.1.21
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/MassiveCore.jar -DgroupId=com.massivecraft.massivecore -DartifactId=MassiveCore -Dversion=2.13.2-SNAPSHOT
+mvn install:install-file -DcreateChecksum=true -Dpackaging=jar -Dfile=lib/Factions.jar -DgroupId=com.massivecraft.factions -DartifactId=Factions -Dversion=2.13.2-SNAPSHOT
